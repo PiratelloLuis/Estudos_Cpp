aca0a272b1d9f85afd00c9dcbdc3169a49692e77
André Luis <45630900+PiratelloLuis@users.noreply.github.com>
2025-10-19 20:24:48 -0300

    if statements

--- ifStatements.cpp
+++ ifStatements.cpp
@@ -7,18 +7,24 @@ int main(){
     std::cout << "Enter your age: " << std::endl;
     std::cin >> age; 
 
-    if (age < 18)
+    if (age >= 21)
     {
-        std::cout << "You can't access the system because you're too young";
+         std::cout << "Welcome to the system! All permissions granted!!";
     }
-
-    else if (age < 21)
+    else if (age >= 18)
     {
         std::cout << "Welcome to the system, but you can't drink anything alcoholic okay?? XD";
+       
     }
+  
+    else if (age < 0)
+    {
+        std::cout << "You haven't been born yet wtf????";
+    }
+    
     else
     {
-        std::cout << "Welcome to the system! All permissions granted!!";
+         std::cout << "You can't access the system because you're too young";
     }
 
 
